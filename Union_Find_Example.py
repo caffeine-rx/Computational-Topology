8236f97ebe1fc1dc05722decc0f27dc594d3f9f2
ajherman <ajherman@pdx.edu>
2015-10-29 18:50:07 -0700

    Revert "Triangulation class"
    This reverts commit 5dab3b4dc3a1c14d0cbd649dc5742ab44f6dc277.

--- Union_Find_Example.py
+++ Union_Find_Example.py
@@ -10,10 +10,10 @@ from random import randint
 # Graph
 vertices = [0,1,2,3,4,5] # Must be a subset of N
 edges = [(0,1),(1,2),(2,4),(4,3),(5,0)] # Must be a relation on N
-num_vertices = len(vertices) # Number of vertices
 
 parent = vertices # parent[i] is the parent of i
 rank = [0 for i in vertices] # if i is a root then rank[i] is the rank of i
+roots = [True for i in vertices] # if i is a root then roots[i] will be True
 
 def Find(i):  # Uses path compression
     if parent[i] == i:
@@ -28,21 +28,19 @@ def Union(i,j):
     if x != y:
         if rank[x] < rank[y]:
             parent[x] = y
+            roots[x]  = False
         elif rank[y] < rank[x]:
             parent[y] = x      
+            roots[y] = False
         else:
             parent[y] = x
+            roots[y] = False
             rank[x] += 1
 
 for i,j in edges:
     Union(i,j)
     
-num_components = 0
-for i in range(num_vertices):
-    if parent[i]==i:
-        num_components += 1
-
-print(num_components) # if this is 1, it means there was only 1 root in the final tree, so G was connected
+print(sum(roots)) # if this is 1, it means there was only 1 root in the final tree, so G was connected
 
     
     
